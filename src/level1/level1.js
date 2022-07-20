941a92f97f37655cb7544c5c6de51c4626450f54
Beto <robledo.mdp@gmail.com>
2022-07-20 10:27:56 -0300

    level1

---
+++ src/level1/level1.js
@@ -0,0 +1,173 @@
+
+// En los próximos 22 problemas, deberás completar la función.
+// Todo tu código irá dentro de las llaves de la función.
+// Asegúrate que usas "return" cuando la consola te lo pida.
+// Pista: "console.log()" NO fucionará.
+// No cambies los nombres de las funciones.
+
+export function devolverString(str) {
+  // Tu código:
+
+  return str.toString();  
+}
+
+export function suma() {
+ // Tu código:
+
+  return;  
+}
+
+export function resta() {
+ // Tu código:
+
+  return;
+}
+
+export function multiplica() {
+  // Tu código:
+
+  return;
+}
+
+export function divide() { 
+  // Tu código:
+
+  return;
+}
+
+export function sonIguales() {  
+  // Tu código:
+
+  return;  
+}
+
+export function tienenMismaLongitud() {  
+  // Tu código:
+
+  return;
+}
+
+export function menosQueNoventa() {  
+  // Tu código:
+
+  return;
+}
+
+export function mayorQueCincuenta() {  
+  // Tu código:
+
+  return;
+}
+
+export function obtenerResto() {  
+  // Tu código:
+
+  return;
+}
+
+export function esPar() {
+  // Tu código:
+
+  return;
+}
+
+export function esImpar() {  
+  // Tu código:
+
+  return;
+}
+
+export function elevarAlCuadrado() {  
+  // Tu código:
+
+  return;
+}
+
+export function elevarAlCubo() { 
+  // Tu código:
+
+  return;
+}
+
+export function elevar() {  
+  // Tu código:
+
+  return;
+}
+
+export function redondearNumero() {  
+  // Tu código:
+
+  return;
+}
+
+export function redondearHaciaArriba() { 
+  // Tu código:
+
+  return;
+}
+
+export function numeroRandom() {  
+  // Tu código:
+
+  return;
+}
+
+export function esPositivo() {
+  // Tu código:
+  
+  return;
+}
+
+export function agregarSimboloExclamacion() {  
+  // Tu código:
+
+  return;
+}
+
+export function combinarNombres() {
+  // Tu código:
+
+  return;
+}
+
+export function obtenerSaludo() {
+  // Tu código:
+
+  return;
+}
+
+export function obtenerAreaRectangulo() {  
+  // Tu código:
+
+  return;
+}
+
+
+export function retornarPerimetro(){
+  // Tu código:
+
+  return;
+}
+
+
+export function areaDelTriangulo(){
+  // Tu código:
+
+  return;
+}
+
+
+export function deEuroAdolar(){
+  //Supongamos que 1 euro equivale a 1.20 dólares.
+  // Tu código:
+
+  return;
+}
+
+
+export function esVocal(){
+  // Tu código:
+
+  return;
+}
