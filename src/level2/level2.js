ddb51859d5ba8a4b24cfa3b4e4206e3821cfa654
Beto <robledo.mdp@gmail.com>
2022-07-22 11:00:50 -0300

    level2

---
+++ src/level2/level2.js
@@ -0,0 +1,94 @@
+// No cambies los nombres de las funciones.
+
+export function obtenerMayor(x, y) {
+  // "x" e "y" son números enteros (int).
+  // Tu código:
+
+  return;
+}
+
+export function mayoriaDeEdad(edad) {
+  // Tu código:
+
+  return;
+}
+  
+export function conection(status) {
+  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
+  // Tu código:
+
+  return;
+}
+
+export function saludo(idioma) {  
+  // Tu código:
+  
+  return;
+}
+
+export function colors(color) {  
+  //Usar el statement Switch.
+  // Tu código:
+ 
+}
+
+export function esDiezOCinco(numero) {
+  // Tu código:
+
+  return;
+}
+
+export function estaEnRango(numero) {  
+  // Tu código:
+
+  return;
+}
+
+export function esEntero(numero) {
+  // Tu código:
+
+  return;
+}
+
+export function fizzBuzz(numero) {
+  // Tu código:
+ 
+  return;
+}
+
+export function operadoresLogicos(num1, num2, num3) {
+  //La función recibe tres números distintos. 
+  // Tu código:
+  
+  return;
+}
+
+export function esPrimo(numero) {
+  /// Tu código:
+
+  return;  
+}
+
+export function esVerdadero(valor){
+  // Tu código:
+
+  return;
+}
+
+export function tablaDelSeis(){
+  // Tu código:
+
+  return;
+}
+
+export function tieneTresDigitos(numero){
+  // Tu código:
+
+  return ;  
+}
+
+export function doWhile(numero) {  
+  //Usar el bucle do ... while.
+  
+  return;
+}
